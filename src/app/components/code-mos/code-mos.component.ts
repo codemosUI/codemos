168a882ae66eeb80fee6580482ce2b04313e8fa6
sswatisharmaa <swati@Swatis-MacBook-Air.local>
2020-04-01 17:00:56 +0530

    Committing starter files

---
+++ src/app/components/code-mos/code-mos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-code-mos',
+  templateUrl: './code-mos.component.html',
+  styleUrls: ['./code-mos.component.css']
+})
+export class CodeMosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
