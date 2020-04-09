681277fda7667c8466085e8e5eafa91167f343c2
sswatisharmaa <swati@Swatis-MacBook-Air.local>
2020-04-09 13:33:58 +0530

    Removed files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CodeMosComponent } from './components/code-mos/code-mos.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CodeMosComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
