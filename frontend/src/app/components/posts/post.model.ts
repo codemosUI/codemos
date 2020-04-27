78e1d6d1b58f7c573ee871396e39e267f37bb5dd
sswatisharmaa <swatisharma1592@gmail.com>
2020-04-27 14:49:21 +0530

    Create post.model.ts

---
+++ frontend/src/app/components/posts/post.model.ts
@@ -0,0 +1,9 @@
+export class Post {
+    postId: number;
+    userId: number;
+    content: string;
+    dateToDisplay: string;
+    createdAt: Date;
+    parentId: number;
+    reactionCount: number;
+}
