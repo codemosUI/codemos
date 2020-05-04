4d1a0c78481fc6518c0ee7963d239e38c61bbb18
Anurag Panday <anuragpanday@Anurags-MacBook-Pro.local>
2020-05-04 17:00:25 +0530

    added google client packages and improved codes

--- server/src/main/java/com/component/dao/impl/GoogleDAOAuthImpl.java
+++ server/src/main/java/com/component/dao/impl/GoogleDAOAuthImpl.java
@@ -1,51 +1,66 @@
-import java.sql.SQLException;
+package com.component.dao.impl;
+
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
+import java.util.*;
 
-package com.component.dao.impl;
 
 public class GoogleDAOAuthImpl {
+	  private HttpTransport transport;
 
-
-	String CLIENT_ID = "1013823904187-21ofld8vn99hljc0t7omqhtvrtd6lv9p.apps.googleusercontent.com";
-
-	// will be received from FE Sign IN request
-	String idTokenString;
+	 private JsonFactory jsonFactory;
+	 private String idTokenString;
+    public void Signup() throws GeneralSecurityException {
+		  String CLIENT_ID = "1013823904187-21ofld8vn99hljc0t7omqhtvrtd6lv9p.apps.googleusercontent.com";
 	
-	
-	GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(HttpTransport transport, JsonFactory jsonFactory)
-	    // Specify the CLIENT_ID of the app that accesses the backend:
-	    .setAudience(CLIENT_ID)
-	    .build();
+			// will be received from FE Sign IN request
+			
+			
+			
+			GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+				    // Specify the CLIENT_ID of the app that accesses the backend:
+				    .setAudience(Collections.singletonList(CLIENT_ID))
+				 
+				    .build();
+			try {
+				GoogleIdToken idToken = verifier.verify(idTokenString); 
+				if (idToken != null) {
+					  Payload payload = idToken.getPayload();
 
-		try {
-			GoogleIdToken idToken = verifier.verify(idTokenString);
-			if (idToken != null) {
-			  Payload payload = idToken.getPayload();
-		
-			  // Print user identifier
-			  String userId = payload.getSubject();
-		
-			  // Get profile information from payload
-			  String email = payload.getEmail();
-			  boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
-			  String name = (String) payload.get("name");
-			  String avatar = (String) payload.get("picture");
-			  String locale = (String) payload.get("locale");
-			  String familyName = (String) payload.get("family_name");
-			  String givenName = (String) payload.get("given_name");
-			// we will insert the data to the database
-	   }
-      else {
-	  System.out.println("Invalid ID token.");
-	}
-  }catch(IOException ioException) {
-	  ioException.printStacktrace();
-  }
+					  // Print user identifier
+					  String userId = payload.getSubject();
+					  System.out.println("User ID: " + userId);
+
+					  // Get profile information from payload
+					  String email = payload.getEmail();
+					  boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+					  String name = (String) payload.get("name");
+					  String pictureUrl = (String) payload.get("picture");
+					  String locale = (String) payload.get("locale");
+					  String familyName = (String) payload.get("family_name");
+					  String givenName = (String) payload.get("given_name");
+					  System.out.println("locale: " + locale);
+					  System.out.println("email: " + email);
+					  System.out.println("pictureUrl: " + pictureUrl);
+					  System.out.println("familyName: " + familyName);
+					  System.out.println("givenName: " + givenName);
+					  System.out.println("emailVerified: " + emailVerified);
+					  System.out.println("name: " + name);
+
+					} else {
+					  System.out.println("Invalid ID token.");
+					}
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			  
+	  }
 }
 
